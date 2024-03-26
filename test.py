628802eff4ecdb6f1d77da0813dce2760096c5b6
diixo <anatolii.podoba@gmail.com>
2024-03-26 16:00:38 +0200

    test.py : implementation stage-2

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 
 import pathlib
 import random
+import string
+import re
+import numpy as np
+
+import keras
+from keras import layers
+from keras.layers import TextVectorization
+import tensorflow.strings as tf_strings
 
 
 text_file = pathlib.Path("spa-eng/spa.txt")
@@ -18,4 +26,48 @@ for line in lines:
 for _ in range(5):
     print(random.choice(text_pairs))
 
+#################################################################
+
+random.shuffle(text_pairs)
+num_val_samples = int(0.15 * len(text_pairs))
+num_train_samples = len(text_pairs) - 2 * num_val_samples
+
+train_pairs = text_pairs[:num_train_samples]
+val_pairs = text_pairs[num_train_samples : num_train_samples + num_val_samples]
+test_pairs = text_pairs[num_train_samples + num_val_samples :]
+
+print(f"{len(text_pairs)} total pairs")
+print(f"{len(train_pairs)} training pairs")
+print(f"{len(val_pairs)} validation pairs")
+print(f"{len(test_pairs)} test pairs")
+
+strip_chars = string.punctuation + "¿"
+strip_chars = strip_chars.replace("[", "")
+strip_chars = strip_chars.replace("]", "")
+
+vocab_size = 15000
+sequence_length = 20
+batch_size = 64
+
+
+def custom_standardization(input_string):
+    lowercase = tf_strings.lower(input_string)
+    return tf_strings.regex_replace(lowercase, "[%s]" % re.escape(strip_chars), "")
+
+
+eng_vectorization = TextVectorization(
+    max_tokens=vocab_size,
+    output_mode="int",
+    output_sequence_length=sequence_length,
+)
+spa_vectorization = TextVectorization(
+    max_tokens=vocab_size,
+    output_mode="int",
+    output_sequence_length=sequence_length + 1,
+    standardize=custom_standardization,
+)
+train_eng_texts = [pair[0] for pair in train_pairs]
+train_spa_texts = [pair[1] for pair in train_pairs]
+eng_vectorization.adapt(train_eng_texts)
+spa_vectorization.adapt(train_spa_texts)
 #################################################################
