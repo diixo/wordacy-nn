4bbdc91412b7b1c894bc539ee8573a48c33af315
diixo <anatolii.podoba@gmail.com>
2024-03-26 15:42:30 +0200

    start implementation test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
 
+import pathlib
+import random
+
+
+text_file = pathlib.Path("spa-eng/spa.txt")
+
+if text_file.exists():
+        fd = open(text_file, 'r', encoding='utf-8')
+        lines = fd.read().split("\n")[:-1]
+
+text_pairs = []
+for line in lines:
+    eng, spa = line.split("\t")
+    spa = "[start] " + spa + " [end]"
+    text_pairs.append((eng, spa))
+
+for _ in range(5):
+    print(random.choice(text_pairs))
+
+#################################################################
